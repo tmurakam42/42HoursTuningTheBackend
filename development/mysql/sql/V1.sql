cc270e07b57355739b9f6df8d848484c57110cae
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2022-04-16 20:48:17 +0900

    s

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,4 +1,4 @@
-ALTER TABLE session ADD INDEX session_value_index (value);
 ALTER TABLE record_comment ADD INDEX record_comment_linked_record_id_index (linked_record_id);
+ALTER TABLE session ADD INDEX session_value_index (value);
 ALTER TABLE record ADD INDEX record_status_index (status);
 ALTER TABLE record ADD INDEX record_created_by (created_by);
