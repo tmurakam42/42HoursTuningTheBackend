73a308c2bacbc1a44f29f05c61d5c869014e2f79
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2022-04-16 18:33:41 +0900

    add record in V1.sql

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,3 @@
 ALTER TABLE session ADD INDEX session_value_index (value);
 ALTER TABLE group_member ADD INDEX group_member_user_id_index (user_id);
+ALTER TABLE record ADD INDEX record_record_id_index (recode_id);
