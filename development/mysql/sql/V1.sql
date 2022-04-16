fc768eb1dc4a2f5315c826cea7a2fa4ded907887
Tamiyasu Murakami <tmurakam@student.42tokyo.jp>
2022-04-16 18:23:00 +0900

    add group_group in V1.sql

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,2 +1,2 @@
 ALTER TABLE session ADD INDEX session_value_index (value);
-ALTER TABLE group_id ADD INDEX group_id_user_id_index (user_id);
+ALTER TABLE group_member ADD INDEX group_member_user_id_index (user_id);
